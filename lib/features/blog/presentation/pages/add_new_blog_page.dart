0361d4460137618c3d6b800313c2be89608e4d74
MeguRobert <megurobi14@gmail.com>
2024-11-02 12:26:51 +0200

    add_new_blog_page design #2

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,3 +1,5 @@
+import 'package:blog_app/core/theme/app_palette.dart';
+import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +14,16 @@ class AddNewBlogPage extends StatefulWidget {
 }
 
 class _AddNewBlogPageState extends State<AddNewBlogPage> {
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    contentController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,25 +33,61 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
           IconButton(onPressed: () {}, icon: Icon(Icons.done_rounded)),
         ],
       ),
-      body: Column(
-        children: [
-          DottedBorder(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            DottedBorder(
+              color: AppPalette.borderColor,
+              dashPattern: const [10, 4],
+              radius: const Radius.circular(10),
+              borderType: BorderType.RRect,
+              strokeCap: StrokeCap.round,
               child: Container(
-            height: 150,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.folder_open, size: 40),
-                SizedBox(height: 15),
-                Text(
-                  "Select your image",
-                  style: TextStyle(fontSize: 15),
+                height: 150,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.folder_open, size: 40),
+                    SizedBox(height: 15),
+                    Text(
+                      "Select your image",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ))
-        ],
+            const SizedBox(height: 20),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  "Bussiness",
+                  "Technology",
+                  "Programming",
+                  "Entertainment",
+                ]
+                    .map(
+                      (e) => Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Chip(
+                          label: Text(e),
+                          side: const BorderSide(
+                            color: AppPalette.borderColor,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+            BlogEditor(controller: titleController, hintText: "Blog Title"),
+            SizedBox(height: 8),
+            BlogEditor(controller: contentController, hintText: "Blog Content"),
+          ],
+        ),
       ),
     );
   }
